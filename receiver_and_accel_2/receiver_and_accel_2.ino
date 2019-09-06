b94fb05efb7cdd9361d1de0a70bff5b20e31dbd2
ANGRY_ELECTRONICAN <1Google2@mail.ru>
2019-09-07 00:10:40 +1000

    Update with sleep

--- receiver_and_accel_2/receiver_and_accel_2.ino
+++ receiver_and_accel_2/receiver_and_accel_2.ino
@@ -1,43 +1,34 @@
 #include <SPI.h>                          // Подключаем библиотеку SPI
-#include <nRF24L01.h>                     // Подключаем библиотеку nRF24L01 
+//#include <nRF24L01.h>                     // Подключаем библиотеку nRF24L01 
 #include <RF24.h>                         // Подключаем библиотеку RF24 
 
-uint8_t data[3];                              // Создаём массив для приёма данных
+uint8_t data[]={0,0,0};                              // Создаём массив для приёма данных
 uint32_t received = 0;
 RF24 radio(9, 10);                        // Указываем номера выводов nRF24L01+ (CE, CSN)
-uint8_t address[] = {0xCC, 0xAA, 0x35};
+uint8_t address[] = {0xCC, 0xAA, 0x35, 0x77, 0xBB};
 
 void setup(void) {
-
   Serial.begin(9600);
   delay(1000);                            // Ждем 1с
-    radio.begin();                          // Инициируем работу nRF24L01+
-    radio.setChannel(100);                  // Указываем канал передачи (от 0 до 126)
-    radio.setDataRate(RF24_1MBPS);         // Указываем скорость передачи (250KBPS, 1MBPS, 2MBPS)
-    radio.setPALevel(RF24_PA_HIGH);         // Указываем мощность передатчика (MIN=-18dBm, LOW=-12dBm, HIGH=-6dBm, MAX=0dBm)
-    radio.openReadingPipe(1, address); // Задаем идентификатор для передачи данных данных
-    radio.startListening();  
-    Serial.println("Radio init!");
-    radio.setPayloadSize(32);
+  radio.begin();                          // Инициируем работу nRF24L01+
+  radio.setChannel(100);                  // Указываем канал передачи (от 0 до 126)
+  radio.setDataRate(RF24_1MBPS);         // Указываем скорость передачи (250KBPS, 1MBPS, 2MBPS)
+  radio.setPALevel(RF24_PA_HIGH);         // Указываем мощность передатчика (MIN=-18dBm, LOW=-12dBm, HIGH=-6dBm, MAX=0dBm)
+  radio.openReadingPipe(1, address); // Задаем идентификатор для передачи данных данных
+  radio.startListening();  
+  Serial.println("Radio init!");
+  radio.setPayloadSize(32);
 }
 
-void loop(void)
-{
-
+void loop(void){
 if(!radio.isChipConnected()) //ошибка или чип не присоединен
   Serial.println("Radio failure!");
-  
   if (radio.available()) {
-    while (radio.available()) {
-      radio.read(data, sizeof(data));
-}
-
-
+    radio.read(data, sizeof(data));
     Serial.println("Received!");
-    for (int i = 0; i < sizeof(data); i++) {
-      Serial.println(data[i]);
-    }
-    
-    if (data[1] == 255) Serial.println("TAP!");
+    uint16_t adc=(data[1]<<8)+data[2];
+    Serial.println(data[0]);
+    Serial.println(adc);
+    Serial.println("");
   } //radio.available endif
 }
--- transiever_and_accel/transiever_and_accel.ino
+++ transiever_and_accel/transiever_and_accel.ino
@@ -1,89 +1,80 @@
-#include <timer_class.h>
 #include <SPI.h>                        // Подключаем библиотеку SPI
 #include <nRF24L01.h>                   // Подключаем библиотеку nRF24L01 
 #include <RF24.h>                       // Подключаем библиотеку RF24 
 #include <Wire.h>
-#include <Adafruit_Sensor.h>
-#include <Adafruit_ADXL345_U.h>
+//#include <Adafruit_Sensor.h>
+#include <LowPower.h>
+//#include <Adafruit_ADXL345_U.h>
 
-byte data[3] = {0, 0, 0};                 // Создаём массив для приёма данных
+byte data[3] = {0,0,0};                 // Создаём массив для приёма данных
 RF24 radio(9, 10);                      // Указываем номера выводов nRF24L01+ (CE, CSN)
-uint8_t adress[] = {0xCC, 0xAA, 0x35};
+uint8_t address[] = {0xCC, 0xAA, 0x35, 0x77, 0xBB};
 
-Timer timer1(10, SECS);
-
-#define INT1_pin 5
-#define ADC_pin A0
-Adafruit_ADXL345_Unified accel = Adafruit_ADXL345_Unified(12345);
+#define INT1_PIN 2
+#define ADC_PIN A0
+uint16_t adc=0;
+//Adafruit_ADXL345_Unified accel = Adafruit_ADXL345_Unified(12345);
+void wakeUp()
+{
+    cli();
+    // accel.readRegister(ADXL345_REG_INT_SOURCE);
+    data[0]=255;
+    ADCSRA|=(1<<ADEN);
+    for(uint8_t i=0;i<8;i++){
+      adc=analogRead(ADC_PIN);
+    }
+    Serial.println(adc);
+    data[1]=highByte(adc);
+    data[2]=lowByte(adc);
+    radio.powerUp();
+    radio.write(data,  sizeof(data));
+    data[0]=0;
+    radio.powerDown();
+}
 
 void setup(void) {
-  pinMode(ADC_pin, INPUT);
+  pinMode(INT1_PIN,INPUT_PULLUP);
+  pinMode(ADC_PIN, INPUT);
   Serial.begin(9600);
+  //Serial.println("START");
   radio.begin();                        // Инициируем работу nRF24L01+
   radio.setChannel(100);                // Указываем канал передачи (от 0 до 126)
   radio.setDataRate (RF24_1MBPS);       // Указываем скорость передачи (250KBPS, 1MBPS, 2MBPS)
   radio.setPALevel(RF24_PA_HIGH);       // Указываем мощность передатчика (MIN=-18dBm, LOW=-12dBm, HIGH=-6dBm, MAX=0dBm)
-  radio.openWritingPipe(adress);// Задаем адрес для передачи данных данных
+  radio.openWritingPipe(address);       // Задаем адрес для передачи данных данных
   radio.setPayloadSize(32);
-  //radio.setRetries(uint8 delay, uint8 count)
-  //delay How long to wait between each retry, in multiples of 250us, max is 15. 0 means 250us, 15 means 4000us.
-  //count How many retries before giving up, max 15
-  if (!accel.begin())
-  {
-    Serial.println("Ooops, no ADXL345 detected ... Check your wiring!");
-    while (1);
-  }
-  pinMode(INT1_pin, INPUT_PULLUP);
-  accel.setRange(ADXL345_RANGE_2_G);
+  radio.stopListening(); 
+  radio.powerDown();
+ /* accel.setRange(ADXL345_RANGE_2_G);
   accel.writeRegister(ADXL345_REG_THRESH_TAP,  0x40);
   accel.writeRegister(ADXL345_REG_DUR,  0x0F);
   accel.writeRegister(ADXL345_REG_TAP_AXES,  0x01);
   accel.writeRegister(ADXL345_REG_DATA_FORMAT,  accel.readRegister(ADXL345_REG_DATA_FORMAT) | (1 << 5));
-  accel.writeRegister(ADXL345_REG_INT_ENABLE,  (1 << 6));
+  accel.writeRegister(ADXL345_REG_INT_ENABLE,  (1 << 6));*/
+  attachInterrupt(0, wakeUp, LOW);
+  sei();  
 }
 
 void loop(void)
-{
-  if (Serial.available() > 0 and Serial.read() == 's' ) { // when 's' typed
-    //check errors
-    if (!radio.isChipConnected()) {
-      Serial.println("Transmitter is not connected!");
+{   
+    //radio.powerDown();
+    sei();
+    for(uint8_t i=0;i<8;i++){
+      LowPower.powerDown(SLEEP_8S, ADC_OFF, BOD_OFF);
     }
-
-    if (accel.getDeviceID() != 0xE5) {
-      Serial.println("Axel error!");
+    ADCSRA|=(1<<ADEN);
+    for(uint8_t i=0;i<8;i++){
+      adc=analogRead(ADC_PIN);
     }
-    
-    data[0] = accel.getDeviceID();
-    data[1] = 0;
-    data[2] = map(analogRead(ADC_pin), 0, 1023, 0, 100); //adc info
-    sensors_event_t event;
-    accel.getEvent(&event);
-    
-    if (digitalRead(INT1_pin) != HIGH) {
-      accel.readRegister(ADXL345_REG_INT_SOURCE);
-      data[1] = 255;
-      Serial.print("TAP ");
-      Serial.println(digitalRead(INT1_pin));
-    }
-    
-    // add ack?
-    radio.write(&data, sizeof(data));               // Отправляем данные из массива data
-
-    while (!radio.txStandBy(1000)) {
-      Serial.println("Transmitter error!");
-      //переделать под попытки переподключиться
-    }
-    
-    Serial.println(analogRead(ADC_pin));
-    Serial.print("Sended ID and TAP status: ");
-          Serial.print("acsel int ");
-      Serial.println(digitalRead(INT1_pin));
-    for (int i = 0; i < sizeof(data); i++) {
-      Serial.println(data[i]);
-    }
-    Serial.println();
-    delay(50);
-  }
-
+    Serial.println(adc);
+    data[1]=highByte(adc);
+    data[2]=lowByte(adc);
+    radio.powerUp();
+    radio.write(data,  sizeof(data));
+    data[0]=0;
+    radio.powerDown();
+    //Serial.println(accel.readRegister(ADXL345_REG_INT_SOURCE),BIN);
+    //Serial.println(datacount++);
+    //Serial.println();
+   // delay(1000);
 }
